f99b61105ddd6168d65fd371cbefb7669321c9c4
Why-Dima <sergienkoda1996@gmail.com>
2021-02-22 15:09:03 +0300

    1

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import datetime
 
 
 class Car(db.Model):
@@ -6,13 +7,25 @@ class Car(db.Model):
     rent_price = db.Column(db.Integer)
     name_auto = db.Column(db.String(128))
     img_url = db.Column(db.String(128))
+    img_url2 = db.Column(db.String(128))
+    img_url3 = db.Column(db.String(128))
+    img_url4 = db.Column(db.String(128))
     transmission = db.Column(db.Boolean, default = True)
-    rent = db.Column(db.String(128))
     description = db.Column(db.String(128))
-    availability = db.Column(db.String(128), default = 'Свободно')
+    availability = db.Column(db.Boolean, default = True)
+    created = db.Column(db.DateTime, default = datetime.now())
+    num_book = db.Column(db.Integer, default = 0)
+    total_time = db.Column(db.Integer, default = 0)
+    total_rent = db.Column(db.Integer, default = 0)
 
-    def transmission(self):
-        if transmission:
-            return True
-        else:
-            return False
+    pr = db.relationship('Journal', backref='car')
+
+class Journal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    auto_info = db.Column(db.Integer, db.ForeignKey('car.id'))
+    time_begin = db.Column(db.DateTime, default = datetime.now())
+    time_end = db.Column(db.DateTime, default = datetime.now())
+    total_cost = db.Column(db.Integer, default = 2)
+
+#uselist=False
+
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from app import app, db
-from app.models import Car
+from app.models import Car, Journal
 from flask import render_template, request
+from datetime import datetime
 
 
 @app.route('/index', methods=['GET', 'POST'])
@@ -18,43 +19,50 @@ def index():
 @app.route('/auto_detail/<int:car_id>', methods=['GET', 'POST'])
 def auto_detail(car_id):
     
-    print('test')
 
     car = Car.query.get(car_id)
 
-    #context = None
-
+    #res = db.session.query(Car, Journal).join(Journal, Car.id == Journal.auto_info).all()
     
-
-    #context = {'id': car.id,
-     #           'transmission': car.transmission,
-      #          'availability': car.availability,
-       #         'name_auto': car.name_auto,
-        #        'price': car.rent_price,
-         #       'auto_description': car.description,
-          #      }
-
-    
-
     return render_template('auto_detail.html', car=car)
 
 
-@app.route('/rent_car/<int:car_id>', methods=['POST','GET'])
+@app.route('/rent_car/<int:car_id>', methods=['GET', 'POST'])
 def rent_car(car_id):
 
-    print('test2')
+    journa = Journal.query.all()
+    #res = db.session.query(Car, Journal).join(Journal, Car.id == Journal.auto_info).all()
+
+
+
+    car = Car.query.get(car_id)
     
     if request.method == 'POST':
 
-        car = Car.query.get(car_id)
-        car.availability = 'Свободно' if car.availability else 'Занято'
-        print(car)
+        
+        car.availability = False if car.availability else True
+        
+        if car.availability == False:
+            car.created = datetime.now() 
+            car.num_book += 1
         db.session.commit()
-    
-    
-    
+        
 
-    return render_template('auto_detail.html', car=car)
+    age_seconds = (datetime.now() - car.created).seconds
+    rent = int(car.rent_price)
+    age1 = age_seconds*rent/60
+    car.total_time += age1
+    car.total_rent = car.total_time * car.rent_price / 60
+    db.session.commit()
+    if car.availability == False:
+        for journal in journa:
+            journal.time_begin = datetime.now()
+            db.session.commit()
+
+
+
+
+    return render_template('auto_detail.html', car=car,age1=age1)
 
 
 
@@ -69,15 +77,24 @@ def create_auto():
         rent_price = request.form['price']
         description = request.form['description']
         transmission = True if request.form['transmission'] == " ДА " else False
+        img_url = request.form['img_url']
+        img_url2 = request.form['img_url2']
+        img_url3 = request.form['img_url3']
+        img_url4 = request.form['img_url4']
 
-        db.session.add(Car(name_auto = name_auto, rent_price = rent_price, description = description, transmission = transmission))
+        db.session.add(Car(name_auto = name_auto, rent_price = rent_price, description = description,
+         transmission = transmission, img_url = img_url, img_url2 = img_url2, img_url3 = img_url3, img_url4 = img_url4))
         db.session.commit()
 
         context = {'method': 'POST', 
                 'name': name_auto,
                 'price': rent_price, 
                 'description': description,
-                'transmission': transmission
+                'transmission': transmission,
+                'img_url': img_url,
+                'img_url2': img_url2,
+                'img_url3': img_url3,
+                'img_url4': img_url4,
                 }
         
     
@@ -92,16 +109,18 @@ def create_auto():
     return render_template('create_auto.html', **context)
 
 
-@app.route('/rental_log')
+@app.route('/rental_log', methods=['GET', 'POST'])
 def rental_log():
     
+    
     car_list = Car.query.all()
+    context = {'car_list':car_list}
 
-    context = {'car_list': car_list}
+
     
     
     
-    return render_template('/rental_log.html', **context)
+    return render_template('rental_log.html', **context)
 
 
 @app.route('/del_auto/<int:car_id>', methods = ['POST'])
@@ -109,11 +128,48 @@ def del_auto(car_id):
     
     car = Car.query.get(car_id)
 
-    print(car)
-
     db.session.delete(car)
     db.session.commit()
 
     return render_template('del_auto.html', car=car)
 
+
+@app.route('/change_auto/<int:car_id>', methods = ['POST', 'GET'])
+def change_auto(car_id):
+
+    car = Car.query.get(car_id)
+
+
+    if request.method == 'POST':
+
+        new_name = request.form['new_name']
+        new_price = request.form['new_price']
+        new_description = request.form['new_description']
+        new_transmission = True if request.form['new_transmission'] == " ДА " else False
+        new_img_url = request.form['new_img_url']
+        new_img_url2 = request.form['new_img_url2']
+        new_img_url3 = request.form['new_img_url3']
+        new_img_url4 = request.form['new_img_url4']
+
+        if new_name:
+            car.name_auto = request.form['new_name']
+        if new_price:
+            car.rent_price = request.form['new_price']
+        if new_description:
+            car.description = request.form['new_description']
+        if new_transmission:
+            car.transmission = request.form['new_transmission']
+        if new_img_url:
+            car.img_url = request.form['new_img_url']
+        if new_img_url2:
+            car.img_url2 = request.form['new_img_url2']
+        if new_img_url3:
+            car.img_url3 = request.form['new_img_url3']
+        if new_img_url4:
+            car.img_url4 = request.form['new_img_url4']
+        
+
+        db.session.commit()
+
+    return render_template('change_auto.html', car=car)
 
